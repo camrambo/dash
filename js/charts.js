d9839173f6d65f5997ab0bc713460296b08aaf04
camrambo <49794860+camrambo@users.noreply.github.com>
2020-05-19 10:36:49 -0600

    Add files via upload

---
+++ js/charts.js
@@ -0,0 +1,310 @@
+//build doughnut chart
+var ctx2 = document.getElementById('myChart2');
+var myChart = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['Sales', 'Upsells', 'Cancellations'],
+        datasets: [{
+            data: [12, 7, 2],
+            backgroundColor: [
+                'rgb(30, 128, 5, 0.4)',
+                'rgb(52, 100, 151, 0.4)',
+                'rgb(145, 0, 0, 0.4)'
+            ],
+            borderColor: [
+                'rgb(30, 128, 5)',
+                'rgb(52, 100, 151)',
+                'rgb(145, 0, 0)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: '# of Units',
+            fontSize: 20
+        }
+    }
+});
+
+
+//build bar chart
+var ctx3 = document.getElementById('myChart3');
+var myChart3 = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: ['Sales', 'Upsells', 'Cancellations'],
+        datasets: [{
+            data: [12000, 3500, 2000],
+            backgroundColor: [
+                'rgb(30, 128, 5, 0.4)',
+                'rgb(52, 100, 151, 0.4)',
+                'rgb(145, 0, 0, 0.4)'
+            ],
+            borderColor: [
+                'rgb(30, 128, 5)',
+                'rgb(52, 100, 151)',
+                'rgb(145, 0, 0)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Total Value',
+            fontSize: 20
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false
+        }
+    }
+});
+
+
+
+//get stock market info last 5 days [WIP2]
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-charts?comparisons=%255EGSPC%252C%255EIXIC&region=US&lang=en&symbol=%255EDJI&interval=1d&range=5d",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-key": "e2eb991334mshc50923f46d3d6a7p1c89cfjsnf351c88d1822"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+    $('.lds-spinner').hide();
+
+    // IN ORDER TO EXPAND THE GRAPH, I WILL NEED TO CHANGE THE 5D RANGE TO 1MO
+    
+    //DOW
+    var oneDayAgoA = response.chart.result[0].indicators.quote[0].close[3];
+    var twoDaysAgoA = response.chart.result[0].indicators.quote[0].close[2];
+    var threeDaysAgoA = response.chart.result[0].indicators.quote[0].close[1];
+    var fourDaysAgoA = response.chart.result[0].indicators.quote[0].close[0];
+
+    //S&P 500
+    var oneDayAgoB = response.chart.result[0].comparisons[0].close[3];
+    var twoDaysAgoB = response.chart.result[0].comparisons[0].close[2];
+    var threeDaysAgoB = response.chart.result[0].comparisons[0].close[1];
+    var fourDaysAgoB = response.chart.result[0].comparisons[0].close[0];
+
+    //Nasdaq
+    var oneDayAgoC = response.chart.result[0].comparisons[1].close[3];
+    var twoDaysAgoC = response.chart.result[0].comparisons[1].close[2];
+    var threeDaysAgoC = response.chart.result[0].comparisons[1].close[1];
+    var fourDaysAgoC = response.chart.result[0].comparisons[1].close[0];
+    
+    // Chart.js setup (stock market graph)
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ['4 Days Ago', '3 Days Ago', '2 Days Ago', 'Yesterday'],
+            datasets: [{
+                label: 'Dow Jones Industrial',
+                borderColor: 'rgb(23, 80, 139)',
+                backgroundColor: 'rgb(23, 80, 139, .28)',
+                pointRadius: 8,
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 2,
+                data: [oneDayAgoA, twoDaysAgoA, threeDaysAgoA, fourDaysAgoA]
+            }, {
+                label: 'S&P 500',
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgb(255, 99, 132, .28)',
+                pointRadius: 8,
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 2,
+                data: [oneDayAgoB, twoDaysAgoB, threeDaysAgoB, fourDaysAgoB],
+                hidden: true
+            }, {
+                label: 'Nasdaq',
+                borderColor: 'rgb(30, 128, 5)',
+                backgroundColor: 'rgb(30, 128, 5, .28)',
+                pointRadius: 8,
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 2,
+                data: [oneDayAgoC, twoDaysAgoC, threeDaysAgoC, fourDaysAgoC],
+                hidden: true
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+
+        }
+    });
+});
+
+//bar chart (3-box section)
+var ctx5 = document.getElementById('myChart5');
+var myChart3 = new Chart(ctx5, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['Employee #1', 'Employee #2', 'Employee #3', 'Employee #4', 'Employee #5'],
+        datasets: [{
+            data: [21, 32, 40, 25, 29],
+            backgroundColor: [
+                'rgb(105, 29, 98, 0.4)',
+                'rgb(52, 100, 151, 0.4)',
+                'rgb(145, 0, 0, 0.4)',
+                'rgb(30, 128, 5, 0.4)',
+                'rgb(198, 91, 124, 0.4)'
+            ],
+            borderColor: [
+                'rgb(105, 29, 98)',
+                'rgb(52, 100, 151)',
+                'rgb(145, 0, 0)',
+                'rgb(30, 128, 5)',
+                'rgb(198, 91, 124)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title: {
+            display: false,
+        },
+        scales: {
+            yAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                ticks: {
+                    suggestedMin: 10
+                },
+                display: false
+            }],
+            xAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                display: false
+            }],
+        },
+        legend: {
+            display: false
+        }
+    }
+});
+
+//line chart (3-box section)
+var ctx4 = document.getElementById('myChart4').getContext('2d');
+    var chart = new Chart(ctx4, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ['', '', '', '', '', '', ''],
+            datasets: [{
+                label: '',
+                borderColor: 'rgb(28, 147, 136)',
+                backgroundColor: 'rgb(28, 147, 136, .28)',
+                pointRadius: 8,
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 2,
+                data: [6, 9, 5, 7, 4, 5, 8]
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        color: "rgba(0, 0, 0, 0)",
+                    },
+                    display: false
+                }],
+                yAxes: [{
+                    gridLines: {
+                        color: "rgba(0, 0, 0, 0)",
+                    },
+                    display: false,
+                    ticks: {
+                        suggestedMin: 3,
+                        max: 11
+                    }
+                }]
+            }
+        }
+    });
+
+
+    //polar chart (3-box section)
+var ctx6 = document.getElementById('myChart6').getContext('2d');
+var chart = new Chart(ctx6, {
+    // The type of chart we want to create
+    type: 'polarArea',
+
+    // The data for our dataset
+    data: {
+        labels: ['', '', '', '', ''],
+        datasets: [{
+            data: [21, 32, 40, 25, 29],
+            backgroundColor: [
+                'rgb(198, 91, 124, 0.4)',
+                'rgb(52, 100, 151, 0.4)',
+                'rgb(145, 0, 0, 0.4)',
+                'rgb(30, 128, 5, 0.4)',
+                'rgb(105, 29, 98, 0.4)'
+            ],
+            borderColor: [
+                'rgb(198, 91, 124)',
+                'rgb(52, 100, 151)',
+                'rgb(145, 0, 0)',
+                'rgb(30, 128, 5)',
+                'rgb(105, 29, 98)'
+            ],
+            borderWidth: 1
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                display: false
+            }],
+            yAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                display: false,
+                ticks: {
+                    suggestedMin: 3,
+                    max: 11
+                }
+            }]
+        }
+    }
+});
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,186 @@
+//sidebar toggle
+$('.xToggle').click(function(){
+    $('.appSidebar').toggleClass('shrink', 1000, 'easeInQuad');
+    $('.appBody').toggleClass('shrink', 1000, 'easeInQuad');
+});
+
+
+//Change the dashboard nodes based on nav option selected
+//My Dashboard (show all)
+$('.mainDash').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $('.dn0').show()
+    $('.dn1').show();
+    $('.dn2').show();
+    $('.dn4').show();
+    $('.dn5').show();
+    $('.dn6').show();
+    $('.dnWide').show();
+    $('.dn7').show();
+    $('.dn8').show();
+    $('.settingsPage').hide();
+});
+//Finance
+$('.financeDash').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $('.dn0').show()
+    $('.dn1').hide();
+    $('.dn2').hide();
+    $('.dn4').hide();
+    $('.dn5').hide();
+    $('.dn6').hide();
+    $('.dnWide').hide();
+    $('.dn7').show();
+    $('.dn8').show();
+    $('.settingsPage').hide();
+});
+//Productivity
+$('.prodDash').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $('.dn0').hide()
+    $('.dn1').show();
+    $('.dn2').show();
+    $('.dn4').show();
+    $('.dn5').show();
+    $('.dn6').show();
+    $('.dnWide').hide();
+    $('.dn7').hide();
+    $('.dn8').hide();
+    $('.settingsPage').hide();
+});
+//Our Team
+$('.teamDash').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $('.dn0').hide()
+    $('.dn1').show();
+    $('.dn2').show();
+    $('.dn4').hide();
+    $('.dn5').hide();
+    $('.dn6').hide();
+    $('.dnWide').show();
+    $('.dn7').hide();
+    $('.dn8').hide();
+    $('.settingsPage').hide();
+});
+//Settings
+$('.settingsDash').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $('.dn0').hide()
+    $('.dn1').hide();
+    $('.dn2').hide();
+    $('.dn4').hide();
+    $('.dn5').hide();
+    $('.dn6').hide();
+    $('.dnWide').hide();
+    $('.dn7').hide();
+    $('.dn8').hide();
+    $('.settingsPage').show();
+});
+//Settings - Change between themes depending on selected button
+//Default Theme
+$('.defaultTheme').click(function() {
+    $('.headingSide').css("background", "#346497");
+    $('.headingMain').css("background", "#17508b");
+    $('.appSidebar').css("background", "#222222");
+    $('.appBody').css("background-color", "#f1f4f6");
+});
+
+//Secondary Theme
+$('.secondaryTheme').click(function() {
+    $('.headingSide').css("background", "#C65B7C");
+    $('.headingMain').css("background", "#F9627D");
+    $('.appSidebar').css("background", "#5b3758");
+    $('.appBody').css("background-color", "#f9ada03d");
+});
+
+
+//grab top news stories from news api
+var request = new XMLHttpRequest();
+request.open('GET', "https://newsapi.org/v2/everything?q=finance&apiKey=ad859ed14dca47bc880b7dad49a524f4");
+request.onload = function() {
+	var response = request.response;
+	var parsedDataFour = JSON.parse(response);
+	//Grab 3 stories
+	for (var i = 0; i < 3; i++) {
+		var NewsTitles = parsedDataFour.articles[i].title;
+		var NewsLinks = parsedDataFour.articles[i].url;
+        // var NewsDescription = parsedDataFour.articles[i].description;
+        // $('.newsBox').append('<a href="' + NewsLinks + '" target="_blank"><div class="newsCard"><div class="cardBody"><h4 class="cardTitle">' + NewsTitles + '</h4></div><div class="cardText">' + NewsDescription + '</div></div></a>');
+        $('.newsBox').append('<a href="' + NewsLinks + '" target="_blank"><div class="newsCard"><div class="cardBody"><h4 class="cardTitle">' + NewsTitles + '</h4></div></div></a>');
+	};
+
+
+};
+request.send();
+
+
+//get current weather data from open weather api
+var requestTwo = new XMLHttpRequest();
+requestTwo.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=draper,utah&appid=2225780abf3f8f7be7e1a3aa2a2a18df&units=imperial');
+requestTwo.onload = function() {
+    var response = requestTwo.response;
+    var parsedData = JSON.parse(response);
+    var cityName = parsedData.name;
+    var currentTemp = parsedData.main.temp;
+    var currentTempSimp = Math.trunc(currentTemp) + '\xB0';
+    var weatherType = parsedData.weather[0].main;
+    var iconCode = parsedData.weather[0].id;
+    $('.weatherBox').append('<div class="cityBox">' + cityName + '</div><div class="tempBox">' + '<i class="wi wi-owm-' + iconCode + '"></i>' + currentTempSimp + '</div><div class="typeBox">' + weatherType + '</div>');
+
+};
+requestTwo.send();
+
+
+//get 3 day forecast from Accuweather (Open Weather's Free API key did not allow for proper forecasting)
+var requestThree = new XMLHttpRequest();
+requestThree.open('GET', 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/2195948?apikey=4dHHaMusAwfapKUomGmtgbuuNKgOgN5e');
+requestThree.onload = function() {
+    var response = requestThree.response;
+    var parsedData = JSON.parse(response);
+    var dayOne = parsedData.DailyForecasts[1];
+    var dayTwo = parsedData.DailyForecasts[2];
+    var dayThree = parsedData.DailyForecasts[3];
+
+    var maxTempOne = Math.trunc(dayOne.Temperature.Maximum.Value) + '\xB0';
+    var maxTempTwo = Math.trunc(dayTwo.Temperature.Maximum.Value) + '\xB0';
+    var maxTempThree = Math.trunc(dayThree.Temperature.Maximum.Value) + '\xB0';
+
+    var minTempOne = Math.trunc(dayOne.Temperature.Minimum.Value) + '\xB0';
+    var minTempTwo = Math.trunc(dayTwo.Temperature.Minimum.Value) + '\xB0';
+    var minTempThree = Math.trunc(dayThree.Temperature.Minimum.Value) + '\xB0';
+
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var x = weekday[d.getDay()+1];
+    var y = weekday[d.getDay()+2];
+    var z = weekday[d.getDay()+3];
+
+    $('.fcOne').append('<div class="dayName">' + x + '</div><div class="tempForecasts">' + maxTempOne + ' / ' + minTempOne + '</div><div class="tempLabel">High / Low</div>');
+    $('.fcTwo').append('<div class="dayName">' + y + '</div><div class="tempForecasts">' + maxTempTwo + ' / ' + minTempTwo + '</div><div class="tempLabel">High / Low</div>');
+    $('.fcThree').append('<div class="dayName">' + z + '</div><div class="tempForecasts">' + maxTempThree +  ' / ' + minTempThree + '</div><div class="tempLabel">High / Low</div>');
+
+};
+requestThree.send();
+
+
+//customize modal for each employee
+$('.empModal').click(function() {
+    var pic4Modal = $(this).find('.userPIC').html();
+    $('.ePic').html(pic4Modal);
+    
+    var name4Modal = $(this).find('.userNAME').text();
+    $('.eName').text(name4Modal);
+
+    var pos4Modal = $(this).find('.userPOS').text();
+    $('.ePos').text(pos4Modal);
+
+    var ext4Modal = $(this).find('.userEXT').text();
+    $('.eExt').text(ext4Modal);
+});
